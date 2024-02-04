1602baa3a6006b2602a2ebd673a925332e71414a
shruthikumari <shruthikumari221@gmail.com>
2024-02-04 15:57:45 +0530

    init

---
+++ src/StackExample.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);  // Last-in & First-out
+
+        System.out.println(stack);
+        showAndRemoveStackElements(stack);
+    }
+
+    private static void showAndRemoveStackElements(Stack<Integer> stack) {
+        int size = stack.size();
+        for (int i = 1; i < size; i++) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
